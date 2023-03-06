325738a6db8be63445ee3cd6a84f13c900d92e9d
jdanielgoh <jdanielgoh@ciencias.unam.mx>
2023-03-06 17:12:08 -0600

    Agregando analytics

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueAnalytics from 'vue-analytics'
 
+Vue.use(VueAnalytics, {
+  id: 'G-EQSMF5JHT3',
+  router
+})
 Vue.config.productionTip = false
 
 new Vue({
